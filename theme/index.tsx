351adaf8ec12bdd68abf32a01f3427143cb6517b
Nick Doherty <nick@nickdoherty.com>
2025-04-24 14:32:30 +1000

    chore: upgraded to rspress v2 beta, adjusted theme accordingly, fixed a dead link

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,13 +1,10 @@
-import Theme from "rspress/theme";
+import { Layout as BasicLayout } from 'rspress/theme';
 import "./index.css";
 
 const Layout = () => {
-  return <Theme.Layout beforeNavTitle={<a href="/"><img src="/images/logo.png" width="42" height="42" alt="Rocket Pool" className="mr-4" /></a>} />;
+  return <BasicLayout beforeNavTitle={<a href="/"><img src="/images/logo.png" width="42" height="42" alt="Rocket Pool" className="mr-4" /></a>} />;
 };
 
-export default {
-  ...Theme,
-  Layout,
-};
+export  { Layout };
 
 export * from "rspress/theme";
