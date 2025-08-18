ea85f5bb08649e62706a2cff5bae8dfb42daafbc
Nick Doherty <nick@nickdoherty.com>
2025-08-18 11:17:19 +1000

    feature: new rp brand logos

--- theme/index.tsx
+++ theme/index.tsx
@@ -2,7 +2,7 @@ import { Layout as BasicLayout } from 'rspress/theme';
 import "./index.css";
 
 const Layout = () => {
-  return <BasicLayout beforeNavTitle={<a href="/"><img src="/images/logo.png" width="42" height="42" alt="Rocket Pool" className="mr-4" /></a>} />;
+  return <BasicLayout beforeNavTitle={<a href="/"><img src="/images/logo.svg" width="42" height="42" alt="Rocket Pool" className="mr-4" /></a>} />;
 };
 
 export  { Layout };
